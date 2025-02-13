f5bc1a75d7c56ca83179138967b8b45112343935
jmalarconn <jmalarcon@ucundinamarca.edu.co>
2025-02-13 17:39:16 -0500

    informacion personal

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Julian Mateo Alarcon Ramirez");
+        System.out.println("vengo de Pacho Cundinamarca");
+    }
+}
